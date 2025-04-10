583ffa02f24b4723ddbca5909adead10d6f3855d
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2025-04-10 15:55:20 +0530

    bestSeller component done

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 // Create a context for the app
 // This context will be used to share state and functions across the application
@@ -7,11 +9,57 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
 
+    const currency= import.meta.env.VITE_CURRENCY;
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSellar, setIsSellar] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([])
 
+    const [cartItems, setCartItems] = useState({})
+
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    }
+
+    const addToCard= (itemId) => {
+        let cardData= structuredClone(cartItems);
+
+        if(cardData[itemId]){
+            cardData[itemId] +=1;
+        }else{
+            cardData[itemId]=1;
+        }
+        setCartItems(cardData);
+        toast.success("added to cart");
+    }
+
+    const updateCardItems = () => {
+        let cardData= structuredClone(cartItems);
+        cardData[itemId] = quantity;
+        setCartItems(cardData);
+        toast.success("cart updated");
+    }
+
+    const removeFromCart = (itemId) => {
+        let cardData= structuredClone(cartItems);
+
+        if(cardData[itemId]){
+            cardData[itemId] -=1;
+
+            if(cardData[itemId]===0){
+                delete cardData[itemId];
+            }
+        }
+        toast.success("removed from cart");
+        setCartItems(cardData);
+        
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+    
     const value = {
         navigate,
         user,
@@ -20,6 +68,12 @@ export const AppContextProvider = ({ children }) => {
         setIsSellar,
         showUserLogin,
         setShowUserLogin,
+        products,
+        currency,
+        addToCard,
+        updateCardItems,
+        removeFromCart,
+        cartItems
     };
 
     return (
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
 const Home = () => {
   return (
     <div className='pt-10'>
-      <MainBanner/>
-        <Categories/>
+      <MainBanner />
+      <Categories />
+      <BestSeller />
     </div>
   )
 }
