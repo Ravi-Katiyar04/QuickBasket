9599c3781b336000233c4673803c9f3986562a98
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2025-04-10 21:10:34 +0530

    footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import { Routes, Route, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import {Toaster} from 'react-hot-toast'
 
 const App = () => {
@@ -27,6 +28,7 @@ const App = () => {
         </Routes>
       </div>
 
+      {isSellerPath ? null : <Footer/>} 
     </div>
   )
 }
