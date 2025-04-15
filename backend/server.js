a14964638a8b4a7d06dce4030ae5221ba4a326ce
Ravi-Katiyar04 <drxravikatiyar0000@gmail.com>
2025-04-15 12:50:14 +0530

    backend setup done

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors'
+import connectDB from './configs/db.js';
+import 'dotenv/config';// Load environment variables from .env file
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+await connectDB(); // Connect to MongoDB
+
+const allowedOrigins = [
+    'http://localhost:5173', // React frontend
+];
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true })); // Allow requests from the frontend
+
+app.get('/', (req, res) => {
+    res.send(`Hello World! This is the backend server.`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
